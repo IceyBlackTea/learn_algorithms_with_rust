ed84ba21b54bf4e7df45a34f9c607481740e4e3a
IceyBlackTea <One_Random@outlook.com>
2021-03-19 00:52:05 +0800

    remake binary tree with rc & cell

--- src/tree/binary.rs
+++ src/tree/binary.rs
@@ -2,98 +2,128 @@
  * @Author: One_Random
  * @Date: 2021-03-17 06:18:19
  * @LastEditors: One_Random
- * @LastEditTime: 2021-03-17 07:11:06
+ * @LastEditTime: 2021-03-19 00:44:05
  * @FilePath: \learn_algorithms_with_rust\src\tree\binary.rs
  * @Description: Copyright © 2020 One_Random. All rights reserved.
  */
+
+use std::rc::Rc;
+use std::cell::{ Ref, RefCell, RefMut };
+
+pub enum Postion{
+    LEFT,
+    RIGHT,
+}
+
+pub enum Traversal{
+    PREORDER,
+    INORDER,
+    POSTORDER,
+    LEVEL,
+}
 
 #[derive(PartialEq, Clone, Debug)]
 pub struct BinaryNode<T>
 where
     T: Copy + PartialOrd + std::fmt::Display, 
 {
-    left_child: Option<Box<BinaryNode<T>>>,
-    sibling: Option<Box<BinaryNode<T>>>,
-    data: T
+    left: Option<Rc<RefCell<BinaryNode<T>>>>,      // child
+    right: Option<Rc<RefCell<BinaryNode<T>>>>,     // sibling
+    value: T
 }
 
 impl<T> BinaryNode<T> 
 where
     T: Copy + PartialOrd + std::fmt::Display,
 {
-    pub fn new(data: &T) -> BinaryNode<T> {
+    // fn get_rc(rc: &Option<Rc<RefCell<BinaryNode<T>>>>) -> Option<Rc<RefCell<BinaryNode<T>>>> {
+    //     if let Some(ref rf_node) = *rc {
+    //         let rc = Rc::clone(rf_node);
+    //         Some(rc)
+    //     } else {
+    //         None
+    //     }
+    // }
+
+    pub fn new(value: &T) -> BinaryNode<T> {
         BinaryNode {
-            left_child: None,
-            sibling: None,
-            data: data.clone()
+            left: None,
+            right: None,
+            value: value.clone()
         }
+    }
+
+    pub fn set_value(&mut self, new_value: &T) {
+        self.value = new_value.clone();
     }
     
-    pub fn get_data(&self) -> T {
-        return self.data;
+    pub fn get_value(&self) -> T {
+        return self.value;
     }
 
-    pub fn get_left_child(&mut self) -> Option<&mut BinaryNode<T>> {
-        match self.left_child {
-            Some(ref mut node) => Some(node),
+    pub fn get_left(&self) -> Option<Ref<BinaryNode<T>>> {
+        match self.left {
+            Some(ref node) => Some(node.borrow()),
             None => None
         }
     }
 
-    pub fn get_right_child(&mut self) -> Option<&mut BinaryNode<T>> {
-        match self.get_left_child() {
-            Some(left_child) => {
-                match left_child.get_sibling() {
-                    Some(right_child) => Some(right_child),
-                    None => None,
-                }
-            },
+    pub fn get_mutable_left(&mut self) -> Option<RefMut<BinaryNode<T>>> {
+        match self.left {
+            Some(ref mut node) => Some(node.borrow_mut()),
             None => None
         }
     }
 
-    pub fn get_sibling(&mut self) -> Option<&mut BinaryNode<T>> {
-        match self.sibling {
-            Some(ref mut node) => Some(node),
+    pub fn get_right(&self) -> Option<Ref<BinaryNode<T>>> {
+        match self.right {
+            Some(ref node) => Some(node.borrow()),
             None => None
         }
     }
 
-    // fn get_parent(&mut self, root: &mut BinaryNode<T>) -> Option<&mut BinaryNode<T>> {
-    //     return Some(root);
-    //     // match root.get_left_child() {
-    //     //     Some(left_child) => {
-    //     //         if self == left_child {
-    //     //             return Some(root);
-    //     //         } else {
-    //     //             return left_child.get_parent(root);
-    //     //         }
-    //     //     },
-    //     //     _ => {}
-    //     // };
+    pub fn get_mutable_right(&mut self) -> Option<RefMut<BinaryNode<T>>> {
+        match self.right {
+            Some(ref mut node) => Some(node.borrow_mut()),
+            None => None
+        }
+    }
 
-    //     // match root.get_sibling() {
-    //     //     Some(sibling) => {
-    //     //         if self == sibling {
-    //     //             return sibling.get_parent(root);
-    //     //         }
-    //     //     },
-    //     //     _ => {}
-    //     // }
+    // fn get_parent<'a>(&mut self, root: &'a mut BinaryNode<T>) -> Option<&'a mut BinaryNode<T>> {
+    //     match root.get_left() {
+    //         Some(left) => {
+    //             if self == left {
+    //                 return Some(root);
+    //             } else {
+    //                 return left.get_parent(root);
+    //             }
+    //         },
+    //         _ => {}
+    //     };
+
+    //     match root.get_right() {
+    //         Some(right) => {
+    //             if self == right {
+    //                 return right.get_parent(root);
+    //             }
+    //         },
+    //         _ => {}
+    //     }
 
     //     return None;
     // }
 
-    pub fn append(&mut self, child: &BinaryNode<T>) {
-        match self.left_child {
+    pub fn append(&mut self, new_node: &BinaryNode<T>) {
+        match self.left {
             None => {
-                self.left_child = Some(Box::new(child.clone()));
+                self.left = Some(Rc::new(RefCell::new(new_node.clone())));
                 return;
             },
             Some(ref mut node) => {
-                match node.sibling {
+                let mut child = node.borrow_mut();
+                match child.right{
                     None => {
-                        node.sibling = Some(Box::new(child.clone()));
+                        child.right = Some(Rc::new(RefCell::new(new_node.clone())));
                     },
                     _ => {
                         panic!("No, It already has two children!");
@@ -104,36 +134,50 @@ where
     }
 
     pub fn remove(&mut self) {
-        match self.left_child {
+        match self.left {
             None => {
                 panic!("No, It doesn't have a child yet!");
             },
             _ => {
-                self.left_child = None;
+                self.left = None;
             }
         }
     }
 
     pub fn is_leaf(&self) -> bool {
-        match self.left_child {
+        match self.left {
             None => true,
             _ => false
         }
     }
 
-    pub fn preorder_traversal(root: &BinaryNode<T>) {
-        println!{"Node value is {}", root.get_data()};
+    pub fn traversal(&self, traversal_type: Traversal) {
+        let self_ref = RefCell::new(self.clone());
+        let root = self_ref.borrow();
 
-        match root.left_child {
+        match traversal_type {
+            Traversal::PREORDER => {
+                BinaryNode::preorder_traversal(root);
+            }
+            _ => {
+                panic!("para is wrong!");
+            }
+        }
+    }
+
+    fn preorder_traversal(root: Ref<BinaryNode<T>>) {
+        println!{"Node value is {}", root.get_value()};
+
+        match root.left {
             Some(ref node) => {
-                BinaryNode::preorder_traversal(node)
+                BinaryNode::preorder_traversal(node.borrow())
             },
             _ => {}
         }
 
-        match root.sibling {
+        match root.right {
             Some(ref node) => {
-                BinaryNode::preorder_traversal(node)
+                BinaryNode::preorder_traversal(node.borrow())
             },
             _ => {}
         }
--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -2,11 +2,11 @@
  * @Author: One_Random
  * @Date: 2021-03-17 06:18:10
  * @LastEditors: One_Random
- * @LastEditTime: 2021-03-17 06:19:11
+ * @LastEditTime: 2021-03-19 00:45:08
  * @FilePath: \learn_algorithms_with_rust\src\tree\mod.rs
  * @Description: Copyright © 2020 One_Random. All rights reserved.
  */
 
 mod binary;
 
-pub use binary::BinaryNode;
+pub use binary::{ BinaryNode, Postion, Traversal};
