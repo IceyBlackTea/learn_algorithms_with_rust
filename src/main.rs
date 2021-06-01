0fc255c08d4d381384d651a95cd991d0819fbad8
IceyBlackTea <One_Random@outlook.com>
2021-06-01 15:11:53 +0800

    🎨 style(main): update codes

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
  * @Author: One_Random
  * @Date: 2021-03-14 20:50:52
  * @LastEditors: One_Random
- * @LastEditTime: 2021-03-19 09:41:45
- * @FilePath: \learn_algorithms_with_rust\src\main.rs
+ * @LastEditTime: 2021-06-01 15:07:29
+ * @FilePath: /learn_algorithms_with_rust/src/main.rs
  * @Description: Copyright © 2020 One_Random. All rights reserved.
  */
 
@@ -12,44 +12,38 @@
 //     hello::print_hello();
 // }
 
-// mod sort;
-// fn sort_test() {
-//     let array = vec![5, 3, 4, 8];
-//     let insertion_sorted_array = sort::insertion_sort(&array);
-//     let merge_sorted_array = sort::merge_sort(&array);
-//     println!("{:?}", array);
-//     println!("{:?}", insertion_sorted_array);
-//     println!("{:?}", merge_sorted_array);
-// }
+mod sort;
+fn sort_test() {
+    let array = vec![5, 3, 4, 8];
+    let insertion_sorted_array = sort::insertion_sort(&array);
+    let merge_sorted_array = sort::merge_sort(&array);
+    println!("{:?}", array);
+    println!("{:?}", insertion_sorted_array);
+    println!("{:?}", merge_sorted_array);
+}
 
-// fn main() {
-//     hello_test();
-//     sort_test();
-// }
+// mod tree;
+// use tree::{ BinaryNode, Postion, Traversal};
 
-mod tree;
-use tree::{ BinaryNode, Postion, Traversal};
+// fn tree_test() {
+//     let mut root = BinaryNode::new(&1);
+    
+//     let leaf_a = BinaryNode::new(&2);
+//     let leaf_b = BinaryNode::new(&3);
+//     let leaf_c = BinaryNode::new(&4);
+
+//     root.append(&leaf_a);
+//     root.append(&leaf_b);
+
+//     root.get_mutable_next_node(Postion::LEFT).unwrap()
+//         .get_mutable_next_node(Postion::RIGHT).unwrap()
+//         .append(&leaf_c);
+
+//     // root.remove_child_node(Postion::RIGHT);
+
+//     root.traversal(Traversal::LEVEL);
+// }
 
 fn main() {
-    let mut root = BinaryNode::new(&1);
-    
-    let leaf_a = BinaryNode::new(&2);
-
-    let leaf_b = BinaryNode::new(&3);
-
-    let leaf_c = BinaryNode::new(&4);
-
-    root.append(&leaf_a);
-
-    root.append(&leaf_b);
-
-    root.get_mutable_next_node(Postion::LEFT).unwrap()
-        .get_mutable_next_node(Postion::RIGHT).unwrap()
-        .append(&leaf_c);
-
-    // root.remove_child_node(Postion::RIGHT);
-
-    root.traversal(Traversal::LEVEL);
-    
-    // println!("{:?}", root);
+    sort_test();
 }
